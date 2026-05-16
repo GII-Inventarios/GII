57a789667521ed4b45d0f8e04a806223468b144d
GII-Inventarios <julianyt63@gmail.com>
2026-05-15 21:17:16 -0500

    Update config.js

--- Recursos/Drivers/config.js
+++ Recursos/Drivers/config.js
@@ -1,13 +1,19 @@
-
 const config = {
-    tipo: "inventarios", ✅
+
+    tipo: "inventarios",
 
     campos: [
+
         { name: "nombre", label: "Nombre", type: "text" },
+
         { name: "marca", label: "Marca", type: "text" },
+
         { name: "precio", label: "Precio", type: "number" },
+
         { name: "cantidad", label: "Cantidad", type: "number" }
+
     ],
 
     idField: "id"
+
 };
